8e201e604beb97639e52bc5f199cb17e27894867
Jamil-Ibrahimli~ <ibrahimlicemil@example.com>
2023-11-28 03:13:27 +0400

    modified

--- JS 4 TASK Algorithms/main.js
+++ JS 4 TASK Algorithms/main.js
@@ -127,5 +127,209 @@
 
 
 
-// 6) Write program to find the count of each character: 'abacddbec' => {a: 2, b: 2, c: 1, d: 3, e: 1}   
 
+// 6) Write program to find the count of each character: 'abacddbec' => {a: 2, b: 2, c: 1, d: 3, e: 1}
+
+
+// let inputWord = 'abacddbec'
+
+
+// function countChar(str){
+
+//   let obj = { }
+
+//   for(let i of str){
+
+// if(obj[i]){
+
+// obj[i]++
+
+// }
+// else{ obj[i]=1}
+//   }
+
+// return obj
+
+// }
+
+// console.log(countChar(inputWord))
+
+
+
+
+
+// 7)Write program that truncates string in a given length: 'Hello world', 5 => 'Hello...'    
+
+
+// let str = "Hello World"
+
+
+
+// function cutString() {
+
+//     let cutted = ' '
+
+//     for (let i = 0; i < str.length; i++) {
+
+//         if (i <= 5) {
+
+//             cutted += str[i]
+
+//         }
+//         else { cutted += "...";
+
+//     break}
+
+
+
+//     }
+
+//     return cutted
+
+// }
+
+// console.log(cutString())
+
+
+
+
+
+// 8) Write program that truncates string in a given length, but it should not break the word: 'Hello world, nice talking to you', 20 => 'Hello world, nice...' 
+
+
+// let str = 'Hello world, nice talking to you'
+
+// let maxLength = Number(prompt("Enter desired length"))
+
+// function catString(x) {
+
+//     let cutted = ' '
+
+//     let splitted = str.split(' ');
+
+//     for (let i = 0;i<splitted.length;i++){
+
+// if(cutted.length+splitted[i].length<x){
+
+// cutted += splitted[i]+' ';
+
+// }
+// else{ cutted+='...';
+// break;
+
+// }
+//     }
+
+// return cutted
+
+// }
+
+
+// console.log(catString(maxLength))
+
+
+
+
+// 9) Create a program to calculate the sum of the elements in an array of numbers: [1, 2, 3, 4, 5] => 15    
+
+// let numbers = [1, 2, 3, 4, 5]
+
+
+
+// function getSum(arr) {
+
+//     let sum = 0
+//     for (let i of arr) {
+
+
+//         sum += i
+
+//     }
+
+
+//     return "the sum is"+" "+ sum
+
+
+// }
+
+// console.log(getSum(numbers))
+
+
+
+// 10)Create a program to calculate the average of the elements in an array of numbers: [1, 2, 3, 4, 5] => 3   
+
+
+
+// const numbers = [1,2,3,4,5];
+
+// function getAverage(arr){
+//  let average = 0;
+
+//  for(let i of arr){
+
+// average+=i
+
+
+//  }
+
+
+//  return average/arr.length
+// }
+
+// console.log("The average is"+' '+getAverage(numbers) )
+
+
+
+// 11)  Create a program to find the maximum number in an array of numbers: [1, 2, 3, 4, 5] => 5    
+
+// let numbers = [1,2,3,4,5];
+
+// function findMax (arr){
+// let maxNum = 0 ;
+
+// for(let i=0;i<arr.length;i++){
+
+// if(arr[i]>maxNum){
+
+// maxNum=arr[i]
+
+// }
+
+// else ( maxNum="Not accept")
+
+// }
+
+
+// return maxNum
+
+// }
+
+
+
+// console.log(findMax(numbers))
+
+
+
+// 12) Create a program to find the minimum number in an array of numbers: [1, 2, 3, 4, 5] => 1    
+
+
+// let numbers = [1,2,3,4,5]
+
+// function findMin(arr){
+
+//  let minNum = arr[0]   
+
+// for(let i = 0;i<arr.length;i++){
+
+// if(minNum>arr[i]){ minNum=arr[i]  }
+
+// }
+
+// return minNum
+
+// }
+
+
+
+
+// console.log(findMin(numbers))
