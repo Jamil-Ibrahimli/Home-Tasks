1538c92348a676512671d3cf22bf25643e62e474
Jamil-Ibrahimli~ <ibrahimlicemil@example.com>
2024-01-21 02:40:57 +0400

    Images imported

--- react_test2/src/components/banner/Banner.jsx
+++ react_test2/src/components/banner/Banner.jsx
@@ -1,9 +1,9 @@
 import './banner.css'
-
+import img1 from '../../assets/tianyi-ma-WiONHd_zYI4-unsplash.jpg'
 const Banner = () => {
   return (
     <div className='banner'>
-      <img src="src/assets/tianyi-ma-WiONHd_zYI4-unsplash.jpg" alt="MacBook banner" className='banner_image'/>
+      <img src={img1} alt="MacBook banner" className='banner_image'/>
     </div>
   )
 }
--- react_test2/src/layout/Main.jsx
+++ react_test2/src/layout/Main.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Banner from '../components/banner/Banner'
 import Card from '../components/card/Card'
-import './main.css'
+import './main.css';
+import img1 from '../assets/arif-riyanto-vJP-wZ6hGBg-unsplash.jpg';
+import img2 from '../assets/clement-helardot-95YRwf6CNw8-unsplash.jpg';
+import img3 from '../assets/danial-igdery-FCHlYvR5gJI-unsplash.jpg';
+import  img4 from '../assets/florian-olivo-4hbJ-eymZ1o-unsplash.jpg';
+import img5 from '../assets/tianyi-ma-WiONHd_zYI4-unsplash.jpg';
+import img6 from '../assets/goran-ivos-iOykDIkZLQw-unsplash.jpg';
+import img7 from '../assets/fotis-fotopoulos-LJ9KY8pIH3E-unsplash.jpg';
+import img8 from '../assets/jefferson-santos-9SoCnyQmkzI-unsplash.jpg';
+
 
 const Main = () => {
 
   const products = [{
 
     title: 'Python: Versatile, readable, and beginner-friendly.',
-    image: 'src/assets/arif-riyanto-vJP-wZ6hGBg-unsplash.jpg',
+    image: img1,
     seen: '124',
     price: '2030',
   },
@@ -16,49 +25,49 @@ const Main = () => {
   {
 
     title: 'JavaScript: Web development powerhouse.',
-    image: 'src/assets/clement-helardot-95YRwf6CNw8-unsplash.jpg',
+    image: img2 ,
     seen: '1234',
     price: '1900',
   },
   {
 
     title: 'Java: Cross-platform, object-oriented stalwart.',
-    image: 'src/assets/danial-igdery-FCHlYvR5gJI-unsplash.jpg',
+    image: img3,
     seen: '22364',
     price: '1000',
   },
   {
 
     title: 'C++: Efficient and widely used for system programming.',
-    image: 'src/assets/florian-olivo-4hbJ-eymZ1o-unsplash.jpg',
+    image: img4,
     seen: '52374',
     price: '3050',
   },
   {
 
     title: 'PHP: Server-side scripting for web applications.',
-    image: 'src/assets/tianyi-ma-WiONHd_zYI4-unsplash.jpg',
+    image: img5,
     seen: '10234',
     price: '400',
   },
   {
 
     title: 'TypeScript: JavaScript with static typing for robust code.',
-    image: 'src/assets/goran-ivos-iOykDIkZLQw-unsplash.jpg',
+    image: img6,
     seen: '234',
     price: '800',
   },
   {
 
     title: 'Go: Fast, simple, and designed for scalability.',
-    image: 'src/assets/fotis-fotopoulos-LJ9KY8pIH3E-unsplash.jpg',
+    image: img7,
     seen: '234',
     price: '100',
   },
   {
 
     title: 'Swift: Apple is language for iOS and macOS development.',
-    image: 'src/assets/jefferson-santos-9SoCnyQmkzI-unsplash.jpg',
+    image: img8,
     seen: '234',
     price: '100',
   },
